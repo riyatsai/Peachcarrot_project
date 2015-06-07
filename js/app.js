d9aeb98d35518ea1ad0f7bbc2139d406568c27a1
Riya <tly@mobagel.com>
2015-06-08 00:04:13 +0800

    PARSE

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+Parse.initialize("fonWQnx1RDOHSy5emNfTb76JOHZtwe37lOJzRnnD","C93Pwv3bwZswToCw4ARTx20hp7ZYLnbNfVcFnQyp");
+
 function showDialog(id){
 	var dialog = $(id).data('dialog');
     dialog.open();
